175cef885ae5d56a0b322793bb07a4fc394800c8
lndslm <lindasalim.ca@gmail.com>
2023-10-08 20:50:33 -0400

    Created the navbar and the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faScissors } from "@fortawesome/free-solid-svg-icons";
+import Moroccanoil_logo from "./Moroccanoil_logo.png";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <nav className="navbar">
+        <div>
+          <a href="#" className="accessibility me-5" title="accessibility">
+            enable accessibility
+          </a>
+          <a href="#" className="country" title="country">
+            canada
+          </a>
+        </div>
+        <div className="d-flex">
+          <a href="#" className="store-locator me-5" title="store locator">
+            <FontAwesomeIcon icon={faLocationDot} /> store locator
+          </a>
+          <a href="#" className="professionals" title="professionals">
+            <FontAwesomeIcon icon={faScissors} rotation={270} /> professionnals
+          </a>
+        </div>
+      </nav>
+      <header className="header sticky-top">
+        <h1>
+          <a href="#" title="Moroccanoil logo">
+            <img src={Moroccanoil_logo} alt="Moroccanoil logo" />
+          </a>{" "}
+        </h1>
+      </header>
     </div>
   );
 }
-
-export default App;
